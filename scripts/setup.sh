3c5ea6b7b7f03aeaa5ad652f7e9a5a62221a34fe
Stefan Landgraf <sl@merrycoder.ch>
2025-02-18 16:07:20 +0100

    Reset

--- scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update
-
-sudo apt install -y python-is-python3 python3-pip
-pip install json-schema-for-humans
-
-chmod +x ./scripts/generate-typescript-model.sh
--- src/ts/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './model/swissrets-model';
-export { validateSwissRetsObject, validateSwissRetsString } from './validator/validator';
-export * from './validator/validator-types';
-// eslint-disable-next-line prettier/prettier
-
