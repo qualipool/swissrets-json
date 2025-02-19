5e195bdab10b39aebfc5d9602b1f7620cc071903
Stefan Landgraf <sl@merrycoder.ch>
2025-02-19 08:42:38 +0100

    fix: fixed main export

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,5 @@
 export * from './model/swissrets-model';
-export {
-  validateSwissRets as validateSwissRetsString,
-  validateSwissRets as validateSwissRetsString
-} from './validator/validator';
+export { validateSwissRets } from './validator/validator';
 export * from './validator/validator-types';
 // eslint-disable-next-line prettier/prettier
 
