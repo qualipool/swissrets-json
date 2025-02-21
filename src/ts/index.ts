b323109062cf8079f1aab667c625e6566ad85d3a
Stefan Landgraf <stefan.landgraf@swissmarketplace.group>
2025-02-21 15:06:13 +0100

    fix: export swissrets-version (#13)
    * fix: export swissrets-version

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,5 @@
 export * from './model/swissrets-model';
+export * from './model/swissrets-version';
 export { validateSwissRets } from './validator/validator';
 export * from './validator/validator-types';
 // eslint-disable-next-line prettier/prettier
